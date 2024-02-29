fff57a251a4984d23efb9f52f766f4d05f22b461
Joe Lollo <53008442+JoeLollo21@users.noreply.github.com>
2024-02-29 11:16:30 -0800

    Add files via upload

---
+++ minecraft/function/command_tool.js
@@ -0,0 +1,35 @@
+import { Server } from "./../../library/Minecraft.js";
+import { Tool } from "./base_tool";
+import { Tools } from "./tool_manager";
+class CommandTool extends Tool {
+    constructor(command) {
+        super();
+        this.isCustom = false;
+        this.use = function (self, player, session) {
+            if (self.isCustom) {
+                const firstSpace = self.command.indexOf(" ");
+                const usingItem = session.usingItem;
+                session.usingItem = false;
+                Server.command.callCommand(player, self.command.substring(0, firstSpace).trim(), self.command.substring(firstSpace).trim());
+                session.usingItem = usingItem;
+            }
+            else {
+                if (player.isOp()) {
+                    Server.queueCommand(self.command, player);
+                }
+            }
+        };
+        if (command.startsWith(";")) {
+            this.isCustom = true;
+            this.command = command.slice(1);
+        }
+        else if (command.startsWith("/")) {
+            this.isCustom = false;
+            this.command = command.slice(1);
+        }
+        else {
+            this.command = command;
+        }
+    }
+}
+Tools.register(CommandTool, "command_wand");
